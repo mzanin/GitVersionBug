d89dc1fa8f14ef96b12ea1579dbf15dc6432300b
mzanin <manuel.zanin@sonova.com>
2021-03-03 15:16:54 +0100

    first

---
+++ src/App.Gui/MainWindow.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace GitVersionBug
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
